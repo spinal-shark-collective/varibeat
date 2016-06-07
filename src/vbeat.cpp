108a34bae3e5257bc1a83ba83faa9ba936b6553f
Colby Klein <shakesoda@gmail.com>
2016-06-07 01:30:01 -0700

    Add judgment

--- src/vbeat.cpp
+++ src/vbeat.cpp
@@ -201,6 +201,9 @@ void add_sprite(video::sprite_batch_t *batch, float x, float y, float *rect = nu
 	batch->add(verts, indices);
 };
 
+static uint32_t good = 200;
+static uint32_t great = 50;
+
 struct notefield_t : widget_t {
 	video::sprite_batch_t *receptors;
 	video::sprite_batch_t *notes;
@@ -211,12 +214,19 @@ struct notefield_t : widget_t {
 
 	double time;
 
-	struct note_data_t {
+	struct note_row_t {
 		uint32_t ms;
 		uint8_t  columns;
 	};
 
-	std::vector<note_data_t> note_data;
+	struct judge_row_t {
+		note_row_t *row;
+		int16_t  offset;
+	};
+
+	std::vector<note_row_t> note_data;
+	std::vector<judge_row_t> judge_data;
+	std::deque<judge_row_t> judging;
 
 	void init() {
 		sampler = bgfx::createUniform("s_tex_color", bgfx::UniformType::Int1);
@@ -270,7 +280,7 @@ struct notefield_t : widget_t {
 			HOLD_MASK  = 0xC0
 		};
 		#define NOTE(x) 1<<x
-		note_data = std::vector<note_data_t> {
+		note_data = std::vector<note_row_t> {
 			{ 100, NOTE(1) | NOTE(4) },
 			{ 250, NOTE(3) },
 			{ 400, NOTE(2) },
@@ -279,7 +289,7 @@ struct notefield_t : widget_t {
 		};
 		#undef NOTE
 
-		time = 0;
+		time = -1;
 	}
 
 	virtual ~notefield_t() {
@@ -290,11 +300,26 @@ struct notefield_t : widget_t {
 	}
 
 	void input(const input_event_t &e) {
-		printf("%d\n", e.key);
+		// cowbell simulator
+		if (e.key == SDLK_SPACE) {
+			int64_t now = uint64_t(this->time * 1000.0);
+			/* anything in this list is guaranteed to be a valid hit, so we just
+			 * need to compute the offset.
+			 *
+			 * TODO: we actually only want to use this input for the closest row */
+			for (auto &jr : this->judging) {
+				if (jr.offset != INT16_MIN) {
+					continue;
+				}
+				int64_t offset = jr.row->ms - now;
+				jr.offset = int16_t(offset);
+				printf("hit! %ldms\n", offset);
+			}
+		}
 	}
 
 	void update(double dt) {
-		time += dt;
+		this->time += dt;
 
 		float speed = 64*3;
 		static float note_rect[] = { 2.f, 2.f, 22.f, 13.f };
@@ -306,12 +331,40 @@ struct notefield_t : widget_t {
 		float height   = 600.f;
 		float spacing  = 6.f;
 		float x_offset = -26.0f;
-		float y_offset = -1000.f;
+		float y_offset = 0.f;
+		uint32_t now = uint32_t(this->time * 1000.0);
+
 		for (auto &row : this->note_data) {
-			double y = height - row.ms + time * speed + y_offset;
+			uint32_t earliest_row = now - good;
+			uint32_t latest_row   = now + good;
+
+			// Send anything out of range into our final judge data.
+			while (!this->judging.empty()
+				&& this->judging.front().row->ms < earliest_row
+			) {
+				judge_row_t &judge_row = this->judging.front();
+				if (judge_row.offset == INT16_MIN) {
+					puts("miss");
+				}
+				this->judge_data.push_back(this->judging.front());
+				this->judging.pop_front();
+			}
+
+			// Add anything ahead in range to our judge list
+			if (row.ms > earliest_row && row.ms < latest_row) {
+				if (this->judging.empty()
+					|| this->judging.back().row->ms < row.ms
+				) {
+					judge_row_t judge_row = { &row, INT16_MIN };
+					this->judging.push_back(judge_row);
+				}
+			}
+
+			double y = height - row.ms + this->time * speed + y_offset;
 			if (y > 0.0) {
 				continue;
 			}
+
 			for (uint8_t i = 1; i < 5; ++i) {
 				uint8_t note = (row.columns >> i) & 0x1;
 				if (!note) {
