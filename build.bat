3d2e06e14bbaa3c06b4dd1388a6b506f2706b391
Colby Klein <shakesoda@gmail.com>
2016-07-03 22:26:51 -0700

    fix the bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 set VS=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 call "%VS%\VC\vcvarsall.bat" x86
-cd scripts
+cd build
 @echo on
 msbuild.exe Varibeat.sln /property:Configuration=Debug /m /t:Varibeat
 cd ..
